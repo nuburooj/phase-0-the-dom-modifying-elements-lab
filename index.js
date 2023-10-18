f5957b383543f73fae2f641f1eb01ec3eb236f36
nuburooj <nuburoojkhattak@gmail.com>
2023-10-18 00:21:03 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+const mainElement = document.getElementById('main');
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement('h1');
+newHeader.textContent = 'Nuburooj';
+newHeader.textContent = 'is the champion';
+newHeader.id = 'victory';
+document.body.appendChild(newHeader);
+
